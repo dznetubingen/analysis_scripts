994436194ee0cd9492dc3ab0ba2694cacb9c4536
KevinMenden <kevin.menden@t-online.de>
2019-07-01 15:10:20 +0200

    Added ChIP-seq analysis folder

---
+++ kevin/rimod-analysis/chipseq/exploratory_analysis.R
@@ -0,0 +1,6 @@
+########################################
+# RiMod ChIP-seq investigative analysis
+########################################
+
+# GRN
+setwd("/media/kevin/89a56127-927e-42c0-80de-e8a834dc81e8/rimod/chipseq/")
