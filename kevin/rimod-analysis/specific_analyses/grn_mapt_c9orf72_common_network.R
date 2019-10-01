0164ba3a312e5c6fe907ee1da28f17a25be572b7
KevinMenden <kevin.menden@t-online.de>
2019-10-01 08:44:11 +0200

    added new analysis

--- kevin/rimod-analysis/specific_analyses/grn_mapt_c9orf72_common_network.R
+++ kevin/rimod-analysis/specific_analyses/grn_mapt_c9orf72_common_network.R
@@ -1,15 +1,14 @@
-#################################
-# Membrane trafficking investigation
-#################################
+##################
+# Analysis of STRING network conncting MAPT, C9orf72 and GRN
+#################
 library(pheatmap)
 library(stringr)
 library(viridis)
 library(biomaRt)
 
-
 ensembl <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
 
-setwd("~/rimod/integrative_analysis/membrane_trafficking//")
+setwd("~/rimod/integrative_analysis/grn_mapt_c9orf72_network/")
 
 # load expression
 mat <- read.table("~/rimod/RNAseq/analysis/RNAseq_analysis_fro_2019-08-12_07.58.35/deseq_vst_values_2019-08-12_07.58.35.txt",
@@ -20,23 +19,25 @@ colnames(mat) <- gsub("X", "", colnames(mat))
 # load md
 md <- read.table("~/rimod/RNAseq/rnaseq_frontal_md.txt", header=T)
 
-# load pathway
-mt <- read.table("membrane_trafficking_reactome.tsv", sep="\t", header=T)
-genes <- str_split(mt$MoleculeName, pattern=" ", simplify = T)[,2]
-mt$name <- genes
+# Load ubiquting pathway
+ubi <- read.table("string_interactions.tsv", sep="\t")
+genes <- as.character(ubi$V1)
+ubi$name <- genes
 # Get ensembl IDs
 bm <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"), filters="hgnc_symbol", values=genes, mart=ensembl)
-mt <- merge(mt, bm, by.x="name", by.y="hgnc_symbol")
-mt <- mt[!duplicated(mt$ensembl_gene_id),]
-rownames(mt) <- mt$ensembl_gene_id
-
+ubi <- merge(ubi, bm, by.x="name", by.y="hgnc_symbol")
+ubi <- ubi[!duplicated(ubi$ensembl_gene_id),]
+rownames(ubi) <- ubi$ensembl_gene_id
+# remove duplicates
+ubi <- ubi[!duplicated(ubi$name),]
 
 # Subset and order the expression matrix
-mat <- mat[rownames(mat) %in% mt$ensembl_gene_id,]
+mat <- mat[rownames(mat) %in% ubi$ensembl_gene_id,]
 mat <- mat[,md$ids]
-mt <- mt[rownames(mat),]
+ubi <- ubi[rownames(mat),]
 # change ensemble ID to hgnc
-rownames(mat) <- mt$name
+rownames(mat) <- ubi$name
+
 
 # Generate heatmap for DEGs
 anno_col <- data.frame(dc = md$mutated_gene)
@@ -46,24 +47,26 @@ rownames(anno_col) <- colnames(mat)
 deg.grn <- read.table("~/rimod/RNAseq/analysis/deconvolution/cell_type_specificity/GRN_cell_composition_filterd_DEGs.txt", sep="\t", header=T)
 mat.grn <- mat[rownames(mat) %in% deg.grn$hgnc_symbol,]
 pheatmap(mat.grn, scale="row", color = viridis(200), annotation_col = anno_col)
-write.table(rownames(mat.grn), "GRN_MembTraf_DEGs.txt", row.names = F, col.names = F, quote=F)
 
 # MAPT
 deg.mapt <- read.table("~/rimod/RNAseq/analysis/deconvolution/cell_type_specificity/MAPT_cell_composition_filterd_DEGs.txt", sep="\t", header=T)
 mat.mapt <- mat[rownames(mat) %in% deg.mapt$hgnc_symbol,]
 pheatmap(mat.mapt, scale="row", color = viridis(200), annotation_col = anno_col)
 
-# Note: no overlap for C9orf72
+# C9orf72 (no overlap)
+deg.c9 <- read.table("~/rimod/RNAseq/analysis/deconvolution/cell_type_specificity/C9orf72_cell_composition_filterd_DEGs.txt", sep="\t", header=T)
+mat.c9 <- mat[rownames(mat) %in% deg.c9$hgnc_symbol,]
+pheatmap(mat.c9, scale="row", color = viridis(200), annotation_col = anno_col)
 
 ###
 # Check for alternative splicing
 ###
 
 as.grn <- read.table("~/rimod/RNAseq/as_analysis/majiq/grn_AS_genes_dPSI_0.2.txt", sep="\t", header = T)
-as.grn <- as.grn[as.grn$x %in% mt$ensembl_gene_id,]
+as.grn <- as.grn[as.grn$x %in% ubi$ensembl_gene_id,]
 
 as.mapt <- read.table("~/rimod/RNAseq/as_analysis/majiq/mapt_AS_genes_dPSI_0.2.txt", sep="\t", header = T)
-as.mapt <- as.character(as.mapt[as.mapt$x %in% mt$ensembl_gene_id,])
+as.mapt <- as.character(as.mapt[as.mapt$x %in% ubi$ensembl_gene_id,])
 
 #===================================#
 
@@ -72,12 +75,12 @@ as.mapt <- as.character(as.mapt[as.mapt$x %in% mt$ensembl_gene_id,])
 ###
 # grn
 ps.grn <- read.table("~/rimod/CAGE/cage_analysis/promotor_shifting/frontal/grn_promotor_shifting_genes_fro.txt", header=T)
-ps.grn <- as.character(ps.grn[ps.grn$x %in% mt$ensembl_gene_id,])
-ps.grn <- mt[ps.grn,]
+ps.grn <- as.character(ps.grn[ps.grn$x %in% ubi$ensembl_gene_id,])
+ps.grn <- ubi[ps.grn,]
 # mapt
 ps.mapt <- read.table("~/rimod/CAGE/cage_analysis/promotor_shifting/frontal/mapt_promotor_shifting_genes_fro.txt", header=T)
-ps.mapt <- as.character(ps.mapt[ps.mapt$x %in% mt$ensembl_gene_id,])
-ps.mapt <- mt[ps.mapt,]
+ps.mapt <- as.character(ps.mapt[ps.mapt$x %in% ubi$ensembl_gene_id,])
+ps.mapt <- ubi[ps.mapt,]
 
 
 #==========================================#
@@ -88,13 +91,13 @@ ps.mapt <- mt[ps.mapt,]
 
 mir.grn <- read.table("~/rimod/smallRNA/frontal/analysis/target_mrna_correlation_analysis_0819/GRN_miRNA_target_edge_table.txt", 
                       sep="\t", header=T, stringsAsFactors = F)
-mir.grn <- mir.grn[mir.grn$targets %in% mt$name,]
-table(mir.grn$mirna)
+mir.grn <- mir.grn[mir.grn$targets %in% ubi$name,]
+table(mir.grn$targets)
 
 mir.mapt <- read.table("~/rimod/smallRNA/frontal/analysis/target_mrna_correlation_analysis_0819/MAPT_miRNA_target_edge_table.txt", 
                        sep="\t", header=T, stringsAsFactors = F)
-mir.mapt <- mir.mapt[mir.mapt$targets %in% mt$name,]
-table(mir.mapt$mirna)
+mir.mapt <- mir.mapt[mir.mapt$targets %in% ubi$name,]
+table(mir.mapt$targets)
 
 
 #============================================#
@@ -117,24 +120,19 @@ getGenes <- function(x){
 mapt.met <- read.table("~/rimod/Methylation/frontal_methylation_0818/DMPs_mapt.ndc_quant.txt", sep="\t")
 mapt.met <- mapt.met[mapt.met$adj.P.Val <= 0.01,]
 mapt.met.genes <- getGenes(mapt.met)
-mapt.met.genes <- mapt.met.genes[mapt.met.genes %in% mt$name]
+mapt.met.genes <- mapt.met.genes[mapt.met.genes %in% ubi$name]
 
 # GRN
 grn.met <- read.table("~/rimod/Methylation/frontal_methylation_0818/DMPs_grn.ndc_quant.txt", sep="\t")
 grn.met <- grn.met[grn.met$P.Value <= 0.01,]
 grn.met.genes <- getGenes(grn.met)
-grn.met.genes <- grn.met.genes[grn.met.genes %in% mt$name]
+grn.met.genes <- grn.met.genes[grn.met.genes %in% ubi$name]
 
 # C9orf72
 c9.met <- read.table("~/rimod/Methylation/frontal_methylation_0818/DMPs_c9orf72.ndc_quant.txt", sep="\t")
 c9.met <- c9.met[c9.met$P.Value <= 0.01,]
 c9.met.genes <- getGenes(c9.met)
-c9.met.genes <- c9.met.genes[c9.met.genes %in% mt$name]
-
-# Further look into WNT6 genes
-#mapt.wnt6 <- mapt.met[grepl("TCF7L2", mapt.met$GencodeBasicV12_NAME),]
-#grn.wnt6 <- grn.met[grepl("WNT6", grn.met$GencodeBasicV12_NAME),]
-#c9.wnt6 <- c9.met[grepl("SORT1", c9.met$GencodeBasicV12_NAME),]
+c9.met.genes <- c9.met.genes[c9.met.genes %in% ubi$name]
 
 #====================================================================#
 
@@ -156,7 +154,7 @@ df <- data.frame(chr=chip.mapt$Chr, start=chip.mapt$Start, end=chip.mapt$End, st
 df <- na.omit(df)
 gr <- makeGRangesFromDataFrame(df)
 mapt.anno <- as.data.frame(annotatePeak(gr, TxDb=txdb, annoDb = "org.Hs.eg.db"))
-mapt.anno <- mapt.anno[mapt.anno$SYMBOL %in% mt$name,]
+mapt.anno <- mapt.anno[mapt.anno$SYMBOL %in% ubi$name,]
 
 # GRN
 chip.grn <- read.table("/media/kevin/89a56127-927e-42c0-80de-e8a834dc81e8/rimod/chipseq/results_grn_narrow/bwa/mergedLibrary/macs/narrowPeak/consensus/H3K4me3/deseq2/H3K4me3.consensus_peaks.results.txt",
@@ -167,7 +165,7 @@ df <- data.frame(chr=chip.grn$Chr, start=chip.grn$Start, end=chip.grn$End, stran
 df <- na.omit(df)
 gr <- makeGRangesFromDataFrame(df)
 grn.anno <- as.data.frame(annotatePeak(gr, TxDb=txdb, annoDb = "org.Hs.eg.db"))
-grn.anno <- grn.anno[grn.anno$SYMBOL %in% mt$name,]
+grn.anno <- grn.anno[grn.anno$SYMBOL %in% ubi$name,]
 
 # C9or72
 chip.c9 <- read.table("/media/kevin/89a56127-927e-42c0-80de-e8a834dc81e8/rimod/chipseq/results_c9orf72_narrow/bwa/mergedLibrary/macs/narrowPeak/consensus/H3K4me3/deseq2/H3K4me3.consensus_peaks.results.txt",
@@ -178,7 +176,7 @@ df <- data.frame(chr=chip.c9$Chr, start=chip.c9$Start, end=chip.c9$End, strand=c
 df <- na.omit(df)
 gr <- makeGRangesFromDataFrame(df)
 c9.anno <- as.data.frame(annotatePeak(gr, TxDb=txdb, annoDb = "org.Hs.eg.db"))
-c9.anno <- c9.anno[c9.anno$SYMBOL %in% mt$name,]
+c9.anno <- c9.anno[c9.anno$SYMBOL %in% ubi$name,]
 
 #==================================#
 
@@ -192,13 +190,12 @@ gene_overlaps <- c()
 for (i in 1:nrow(tf.mapt)) {
   ovl <- as.character(tf.mapt$Overlapping_Genes[i])
   ovl <- str_split(ovl, pattern=",")[[1]]
-  ovl <- ovl[ovl %in% mt$name]
+  ovl <- ovl[ovl %in% ubi$name]
   gene_overlaps <- c(gene_overlaps, length(ovl))
 }
 tf.mapt$wnt_overlap <- gene_overlaps
-#tf.mapt.wnt <- tf.mapt[tf.mapt$TF %in% mt$name,]
 tf.mapt <- tf.mapt[order(tf.mapt$wnt_overlap, decreasing = T),]
-tf.mapt <- tf.mapt[1:20,]
+#tf.mapt <- tf.mapt[1:20,]
 tf.mapt <- tf.mapt[tf.mapt$TF %in% deg.mapt$hgnc_symbol,]
 
 # grn
@@ -208,13 +205,13 @@ gene_overlaps <- c()
 for (i in 1:nrow(tf.grn)) {
   ovl <- as.character(tf.grn$Overlapping_Genes[i])
   ovl <- str_split(ovl, pattern=",")[[1]]
-  ovl <- ovl[ovl %in% wnt$name]
+  ovl <- ovl[ovl %in% ubi$name]
   gene_overlaps <- c(gene_overlaps, length(ovl))
 }
 tf.grn$wnt_overlap <- gene_overlaps
-tf.grn.wnt <- tf.grn[tf.grn$TF %in% mt$name,]
+tf.grn.wnt <- tf.grn[tf.grn$TF %in% ubi$name,]
 tf.grn <- tf.grn[order(tf.grn$wnt_overlap, decreasing = T),]
-tf.grn <- tf.grn[1:20,]
+#tf.grn <- tf.grn[1:20,]
 tf.grn <- tf.grn[tf.grn$TF %in% deg.grn$hgnc_symbol,]
 
 ####
@@ -225,17 +222,20 @@ library(igraph)
 ####
 # Build network for FTD-GRN
 ####
-genes <- as.character(wnt$name)
+
+### grn
+genes <- as.character(ubi$name)
 deg <- deg.grn[deg.grn$hgnc_symbol %in% genes,]
 # get all fold change values as well
 exp.grn <- read.table("~/rimod/RNAseq/analysis/RNAseq_analysis_fro_2019-08-12_07.58.35/deseq_result_grn.ndc_fro_2019-08-12_07.58.35.txt", sep="\t", header=T)
-exp.grn <- exp.grn[exp.grn$X %in% mt$ensembl_gene_id,]
-exp.grn <- merge(exp.grn, mt, by.x="X", by.y="ensembl_gene_id")
+exp.grn <- exp.grn[exp.grn$X %in% ubi$ensembl_gene_id,]
+exp.grn <- merge(exp.grn, ubi, by.x="X", by.y="ensembl_gene_id")
+exp.grn <- exp.grn[, -ncol(exp.grn)]
 # get miRNA fold changes
 exp.mir <- read.table("~/rimod/smallRNA/frontal/analysis/analysis_0719/deseq_result_grn.ndc_frontal_smRNAseq.txt", sep="\t", header=T)
 
 # get PPI connections
-ppi <- read.table("membrane_trafficking_STRING.tsv", sep="\t", header=T)
+ppi <- read.table("string_interactions.tsv", sep="\t", header=T)
 ppi <- ppi[, c(1,2,15)]
 
 # Create edge list from PPIs
@@ -251,13 +251,12 @@ for (i in 1:nrow(mir.grn)){
   edges <- c(edges, e)
 }
 
-# Filter TFs for differentially expressed genes
 
 # Add TF connections to network
 for (i in 1:nrow(tf.grn)) {
   tf <- as.character(tf.grn$TF[i])
   targets <- str_split(tf.grn$Overlapping_Genes[i], pattern = ",")[[1]]
-  targets <- targets[targets %in% wnt$name]
+  targets <- targets[targets %in% ubi$name]
   for (tg in targets){
     e <- c(tf, as.character(tg))
     edges <- c(edges, e)
@@ -307,7 +306,7 @@ for (v in vnames) {
 V(g)$lfc <- lfcs
 
 # Save file
-file_name = "GRN_MembraneTraficking_signaling_network.gml"
+file_name = "GRN_FTD_network.gml"
 write_graph(g, file=file_name, format="gml")
 print("Network creation succesfull.")
 
