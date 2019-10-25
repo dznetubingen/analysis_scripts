16ead8263f4f04429521956dd4a84564e367e0e2
KevinMenden <kevin.menden@t-online.de>
2019-10-25 14:52:30 +0200

    Additional plots

--- kevin/rimod-analysis/figure2_plots.R
+++ kevin/rimod-analysis/figure2_plots.R
@@ -35,13 +35,22 @@ mypal <- brewer.pal(3, "Dark2")
 # Plot Venn
 venn.plot <- venn.diagram(rna.list, 
                           filename = "rnaseq_overlap_venn.png",
+                          cex = 1,
+
+                          # image
                           imagetype = "png",
                           resolution = 300,
-                          fill = mypal,
-                          cex = 1,
-                          cat.cex = 1,
                           height=800,
-                          width=800)
+                          width=800,
+                          
+                          # circles
+                          lty = 'blank',
+                          fill = mypal,
+                          
+                          # names
+                          cat.cex = 1,
+                          cat.pos = c(-27, 27, 135),
+                          cat.dist = c(0.055, 0.055, 0.055))
 
 
 
@@ -72,14 +81,23 @@ cage.list <- list("FTD-MAPT"=rownames(cage.mapt),
 
 # Plot Venn
 venn.plot <- venn.diagram(cage.list, 
-                          filename = "cage_overlap_venn.png",
+                          filename = "cageseq_overlap_venn.png",
+                          cex = 1,
+                          
+                          # image
                           imagetype = "png",
                           resolution = 300,
-                          fill = mypal,
-                          cex = 1,
-                          cat.cex = 1,
                           height=800,
-                          width=800)
+                          width=800,
+                          
+                          # circles
+                          lty = 'blank',
+                          fill = mypal,
+                          
+                          # names
+                          cat.cex = 1,
+                          cat.pos = c(-27, 27, 135),
+                          cat.dist = c(0.055, 0.055, 0.055))
 
 
 
@@ -135,15 +153,114 @@ size <- c(nrow(rna.mapt), nrow(cage.mapt), length(intersect(rownames(rna.mapt), 
           nrow(rna.c9), nrow(cage.c9), length(intersect(rownames(rna.c9), rownames(cage.c9))))
 
 df <- data.frame(DC = group, Method = name, Size = size)
+df$DC <- factor(group, levels = c("FTD-MAPT", "FTD-GRN", "FTD-C9orf72"))
 
-p <- ggplot(df, aes(x=DC, y=Size, fill=Method)) +
-  geom_bar(stat = "identity", position="dodge2")
+p <- ggplot(df, aes(x=Method, y=Size, fill=DC)) +
+  geom_bar(stat = "identity", position="dodge2") +
+  scale_fill_brewer(palette = "Dark2") +
+  facet_wrap(. ~ DC, scales = "free") +
+  theme_minimal(base_size = 15) +
+  theme(axis.text.x = element_text(size = 12, angle = 30))
 p
-ggsave(filename = "cage_rnaseq_barplot.png")
+
+png("cage_rnaseq_barplot.png", height=300, width=500)
+p
+dev.off()
+
+
+#==============================================================#
+
+####
+# String analysis plotting
+####
+setwd("~/rimod/paper/figures/figure2/")
+cutoff <- 12
+# MAPT
+rea.mapt <- read.table("~/rimod/RNAseq/analysis/pathways_analysis/stringdb/mapt_REACT.tsv", sep="\t", header=F, stringsAsFactors = F)
+rea.mapt <- rea.mapt[, 1:6]
+colnames(rea.mapt) <- c("react", "name", "es", "direction", "size", "fdr")
+# change second pathway name
+rea.mapt$name[2] <- rea.mapt$react[2]
+rea.mapt <- rea.mapt[1:cutoff,]
+
+rea.mapt$negLog <- -log10(rea.mapt$fdr)
+rea.mapt <- rea.mapt[order(rea.mapt$negLog),]
+rea.mapt$name <- factor(rea.mapt$name, levels = rea.mapt$name)
+# make barplot
+p <- ggplot(rea.mapt, aes(x=name, y=negLog)) + 
+  geom_bar(stat = "identity", fill = mypal[1]) + 
+  theme_minimal(base_size = 15) +
+  coord_flip() + 
+  scale_fill_continuous()
+p
+
+png("rnaseq_mapt_string_reactome_barplot.png", height=400, width=400)
+p
+dev.off()
+
+
+# GRN
+rea.mapt <- read.table("~/rimod/RNAseq/analysis/pathways_analysis/stringdb/grn.enrichment.RCTM.tsv", sep="\t", header=F, stringsAsFactors = F)
+rea.mapt <- rea.mapt[, 1:6]
+colnames(rea.mapt) <- c("react", "name", "es", "direction", "size", "fdr")
+# change second pathway name
+rea.mapt <- rea.mapt[1:cutoff,]
+rea.mapt$name[5] <- rea.mapt$react[5]
+rea.mapt$name[4] <- rea.mapt$react[4]
+
+
+rea.mapt$negLog <- -log10(rea.mapt$fdr)
+rea.mapt <- rea.mapt[order(rea.mapt$negLog),]
+rea.mapt$name <- factor(rea.mapt$name, levels = rea.mapt$name)
+# make barplot
+p <- ggplot(rea.mapt, aes(x=name, y=negLog)) + 
+  geom_bar(stat = "identity", fill = mypal[2]) + 
+  theme_minimal(base_size = 15) +
+  coord_flip() + 
+  scale_fill_continuous()
+p
+
+png("rnaseq_grn_string_reactome_barplot.png", height=400, width=400)
+p
+dev.off()
+
+#============================================#
+
+####
+# Cell composition plot
+####
+
+fracs <- read.table("~/rimod/RNAseq/analysis/deconvolution/cdn_predictions.txt", sep="\t", header=T)
+colnames(fracs)[1] <- "sample"
+fracs$sample <- gsub("X", "", fracs$sample)
+fracs$sample <- str_split(fracs$sample, pattern="_", simplify = T)[,1]
+fracs$Neurons <- fracs$InNeurons + fracs$ExNeurons
+fracs <- fracs[, c(-3, -9)]
 
 
 
+# get design matrix
+md <- read.csv("~/rimod/files/FTD_Brain.csv")
+md <- md[md$REGION == "frontal",]
+md$sample <- str_split(md$GIVENSAMPLENAME, pattern="_", simplify = T)[,1]
+md <- md[md$sample %in% fracs$sample,]
+md <- md[match(fracs$sample, md$sample),]
+fracs$group <- md$DISEASE.CODE
+
+# remove sporadic
+fracs <- fracs[!fracs$group == "Sporadic-TDP",]
 
 
+# melting
+fracs <- melt(fracs)
 
+p <- ggplot(fracs, aes(x=sample, y=value, fill=variable)) + 
+  geom_bar(stat = "identity") + 
+  facet_grid(cols = vars(group), scales = "free_x", space="free_x") + 
+  theme_minimal(base_size = 15) + 
+  theme(axis.text.x = element_blank())
+p
 
+png("deconvolution_plot_rnaseq.png", height=300, width=500)
+p
+dev.off()
